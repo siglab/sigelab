d88ac0be721141ad398f611bb3dace0ee316efa5
Julián Rodríguez <felorodri@gmail.com>
2019-03-01 18:09:37 -0500

    Login page rework. Ingresar button from home page fixed

--- frontend/sigelabangular/src/app/modulos/mod-principal/inicio-app/inicio-app.component.ts
+++ frontend/sigelabangular/src/app/modulos/mod-principal/inicio-app/inicio-app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 declare var $: any;
 import * as AOS from 'aos';
 
@@ -8,7 +9,7 @@ import * as AOS from 'aos';
   styleUrls: ['./inicio-app.component.css']
 })
 export class InicioAppComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
     $('html, body').animate({ scrollTop: '0px' }, 'slow');
@@ -23,13 +24,17 @@ export class InicioAppComponent implements OnInit {
     );
   }
 
-  goAboutSec() {
+  goAboutSec(): void {
     $('html, body').animate(
       {
         scrollTop: $('#aboutSec').offset().top - $('#topDiv').height() + 5
       },
       1000
     );
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
   }
 
 }
