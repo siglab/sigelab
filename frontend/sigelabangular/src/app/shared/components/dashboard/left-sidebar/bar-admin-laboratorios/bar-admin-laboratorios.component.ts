341b6180a65469edd256b65d81b2dcd210175150
Julián Rodríguez <felorodri@gmail.com>
2019-03-06 20:08:12 -0500

    Sidebar rework done

--- frontend/sigelabangular/src/app/shared/components/dashboard/left-sidebar/bar-admin-laboratorios/bar-admin-laboratorios.component.ts
+++ frontend/sigelabangular/src/app/shared/components/dashboard/left-sidebar/bar-admin-laboratorios/bar-admin-laboratorios.component.ts
@@ -10,51 +10,38 @@ import { Observable } from '@firebase/util';
   styleUrls: ['./bar-admin-laboratorios.component.css']
 })
 export class BarAdminLaboratoriosComponent implements OnInit {
-
   laboratorios2 = [];
   // INICIALIZACION DE CONSULTAS PARA LABORATORIOS
   private labsColection: AngularFirestoreCollection<any>;
   labs: Observable<any[]>;
-
   datosLabsEstructurados = [];
-
   user: any;
   facultades: any;
-
   moduloNivel2 = false;
   moduloPermiso = false;
-
   permisos: any;
 
   constructor(private obs: ObservablesService, private route: Router, private afs: AngularFirestore) { }
 
   ngOnInit() {
-
     if (localStorage.getItem('usuario')) {
       this.getUserId();
       const labs =  JSON.parse(localStorage.getItem('laboratorios'));
-      this.permisos = JSON.parse(localStorage.getItem('permisos'))
-        this.estructurarLaboratorios(labs).then(() => {
-
-          this.laboratorios2 = this.datosLabsEstructurados;
-
-          console.log(this.laboratorios2);
-        });
+      this.permisos = JSON.parse(localStorage.getItem('permisos'));
+      this.estructurarLaboratorios(labs).then(() => {
+        this.laboratorios2 = this.datosLabsEstructurados;
+      });
     }
   }
 
-
   estructurarLaboratorios(labs){
     this.datosLabsEstructurados = [];
-
     let size = 0;
     for (const key in labs) {
       if (labs.hasOwnProperty(key)) {
        size++;
-        
       }
     }
-
     let promise = new Promise((resolve, reject) => {
       for (const key in labs) {
         if (labs.hasOwnProperty(key)) {
@@ -66,44 +53,32 @@ export class BarAdminLaboratoriosComponent implements OnInit {
               labo: elemento,
               roles : this.permisos[key]
             };
-      
             this.datosLabsEstructurados.push(laboratorio);
-
             if(size == this.datosLabsEstructurados.length){
               resolve();
-            } 
-
+            }
           });
-          
         }
       }
     });
-
     return promise;
-  
   }
 
   getLaboratorio(id){
     return this.afs.collection('cfFacil').doc(id).ref.get();
   }
 
-
   getUserId() {
     this.user = JSON.parse(localStorage.getItem('usuario'));
   }
-
-
 
   getPersonId(userid) {
     return this.afs.doc('user/' + userid).ref.get();
   }
 
-
   getPersona(persid) {
     return  this.afs.doc('cfPers/' + persid).snapshotChanges();
   }
-
-
 
   // METODO QUE AJUSTA EL NOMBRE DEL LABORATORIO PARA EL SIDEBAR
   ajustarTexto(nombre) {
@@ -117,7 +92,6 @@ export class BarAdminLaboratoriosComponent implements OnInit {
         name2 += nombreArr[i] + ' ';
       }
     }
-
     return { nom1: name1, nom2: name2 };
   }
 
@@ -132,48 +106,46 @@ export class BarAdminLaboratoriosComponent implements OnInit {
   enviaritemLab(lab) {
     this.obs.changeObjectLab(lab);
   }
+
   enviaritemPer(per) {
     this.obs.changeObjectPer(per);
   }
+
   enviaritemEsp(esp) {
     this.obs.changeObjectEsp(esp);
-   }
+  }
+
   enviaritemPra(pra) {
     this.obs.changeObjectPra(pra);
   }
+
   enviaritemSolServ(solsrv) {
     this.obs.changeObjectSolServ(solsrv);
   }
+
   enviaritemServAsoc(srvasoc) {
     this.obs.changeObjectServAsoc(srvasoc);
   }
+
   enviaritemProy(proy) {
     this.obs.changeObjectProy(proy);
   }
+
   enviaritemSolMante(solman) {
     this.obs.changeObjectSolMante(solman);
   }
+
   enviaritemSolBaja(baja) {
     this.obs.changeObjectSolBaja(baja);
   }
 
-
-
   abrirEnlaceBaja() {
     const url = 'https://swebse12.univalle.edu.co/sabs/paquetes/formatos/fm_bajas.htm';
-
     window.open(url);
   }
 
   abrirEnlaceQuiUv() {
     const url = 'https://dfrq-quiuv.herokuapp.com/cuentas/login/';
-
     window.open(url);
   }
-
-
-
-
-
-
 }
