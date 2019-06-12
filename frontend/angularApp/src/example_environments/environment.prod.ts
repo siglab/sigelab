93f5c98b22f111115ce830c7afc142d811d723d9
francisco <francisco.hurtado@geoprocess.com.co>
2019-06-12 15:01:55 -0500

    add example_environments

---
+++ frontend/angularApp/src/example_environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: 'YOUR_FIREBASE_PRODUCTION_PROJECT_APIKEY',
+    authDomain: 'YOUR_FIREBASE_PRODUCTION_AUTH_DOMAIN',
+    databaseURL: 'YOUR_FIREBASE_PRODUCTION_DATABASE_URL',
+    projectId: 'YOUR_FIREBASE_PRODUCTION_PROJECT_ID',
+    storageBucket: 'YOUR_FIREBASE_PRODUCTION_STORAGE_BUCKED',
+    messagingSenderId: 'YOUR_FIREBASE_PRODUCTION_MESSAGING_SENDER_ID'
+  }
+};
