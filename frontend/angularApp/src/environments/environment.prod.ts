3b167306f4cb1b63f5d53421c6e305dc4b9a4737
francisco <francisco.hurtado@geoprocess.com.co>
2019-09-05 16:10:20 -0500

    reconfigure gitignore

---
+++ frontend/angularApp/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: 'YOUR_FIREBASE_DEVELOPMENT_PROJECT_APIKEY',
+    authDomain: 'YOUR_FIREBASE_DEVELOPMENT_AUTH_DOMAIN',
+    databaseURL: 'YOUR_FIREBASE_DEVELOPMENT_DATABASE_URL',
+    projectId: 'YOUR_FIREBASE_DEVELOPMENT_PROJECT_ID',
+    storageBucket: 'YOUR_FIREBASE_DEVELOPMENT_STORAGE_BUCKED',
+    messagingSenderId: 'YOUR_FIREBASE_DEVELOPMENT_MESSAGING_SENDER_ID'
+  }
+};
